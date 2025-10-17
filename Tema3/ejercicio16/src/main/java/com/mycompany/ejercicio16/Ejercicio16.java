f3435c48a14f1863b292c1984d939aea1bcfc932
cluciap01 <cluciap01@informatica.iesvalledeljerteplasencia.es>
2025-10-17 18:59:16 +0200

    Tema3

---
+++ Tema3/ejercicio16/src/main/java/com/mycompany/ejercicio16/Ejercicio16.java
@@ -0,0 +1,29 @@
+package com.mycompany.ejercicio16;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ejercicio16 {
+
+    public static void main(String[] args) {
+        int contador = 0;
+
+        System.out.println("Los numeros impares existentes entre el numero 20 y el 160 son:");
+
+        // Recorremos desde 21 hasta 159, de 2 en 2
+        for (int i = 21; i < 160; i += 2) {
+            System.out.print(i);
+
+            // Para separar con " – " excepto el último número
+            if (i < 159) {
+                System.out.print(" - ");
+            }
+
+            contador++; // Contamos los impares
+        }
+        
+        System.out.println(); // Salto de línea
+        System.out.println("La cantidad de numeros impares impresos han sido: " + contador);
+    }
+}
