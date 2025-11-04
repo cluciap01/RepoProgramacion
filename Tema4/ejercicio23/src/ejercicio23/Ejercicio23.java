6a46aacfb3c06899d720ec1c6e933cead9abe786
cluciap01 <cluciap01@informatica.iesvalledeljerteplasencia.es>
2025-11-04 12:43:07 +0100

    Tema4-Ejercicio23

---
+++ Tema4/ejercicio23/src/ejercicio23/Ejercicio23.java
@@ -0,0 +1,20 @@
+package ejercicio23;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ejercicio23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    //Declaramos constantes
+    public final static double VELOCIDADLUZ = 299.792;
+    public final static double CONSTANTEUNIVERSALGRAVITACION = 6.67 * 10-11;//elevado a -11
+    public final static double CONSTANTEPLANK = 6.62607015 * 10-34;//elevado a -34
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Tema4/ejercicio23/src/ejercicio23/PruebaConstantes.java
@@ -0,0 +1,13 @@
+package ejercicio23;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PruebaConstantes {
+    public static void main(String[] args) {
+        System.out.println("Velocidad de la luz: " + Ejercicio23.VELOCIDADLUZ + " m/s");
+        System.out.println("Constante de gravitación universal: " + Ejercicio23.CONSTANTEUNIVERSALGRAVITACION + " N*m²/kg2");
+        System.out.println("Constante de Planck: " + Ejercicio23.CONSTANTEPLANK + " J*s");
+}
+}
