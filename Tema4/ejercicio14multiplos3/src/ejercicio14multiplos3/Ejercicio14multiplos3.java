291e62d5ffa8a574f49622b069f55b7904bcb714
cluciap01 <cluciap01@informatica.iesvalledeljerteplasencia.es>
2025-11-04 17:13:59 +0100

    Tema4-Ejercicio14multiplos3

--- Tema4/ejercicio14multiplos3/src/ejercicio14multiplos3/Ejercicio14multiplos3.java
+++ Tema4/ejercicio14multiplos3/src/ejercicio14multiplos3/Ejercicio14multiplos3.java
@@ -19,24 +19,24 @@ public class Ejercicio14multiplos3 {
     // Método para pedir el número al usuario y comprobar que es mayor que 0
     public static int pedirNumero() {
         Scanner entrada = new Scanner(System.in);
-        int n;
+        int num;
         do {
             System.out.print("Introduce un número mayor que 0: ");
-            n = entrada.nextInt();
-            if (n <= 0) {
+            num = entrada.nextInt();
+            if (num <= 0) {
                 System.out.println("Error: el número debe ser mayor que 0.");
             }
-        } while (n <= 0);
-        return n;
+        } while (num <= 0);
+        return num;
     }
 
     // 
     public static void mostrarMultiplosDeTres(int limite) {
         int contador = 0;
-        System.out.println("\nMúltiplos de 3 entre 1 y " + limite + ":");
+        System.out.println("Múltiplos de 3 entre 1 y " + limite + ":");
         for (int i = 1; i <= limite; i++) {
             if (i % 3 == 0) {
-                System.out.print(i + " ");
+                System.out.println(i + " ");
                 contador++;
             }
         }
