d7c37b4b5e07f38d4b0ae688598b80a6a0e2a684
cluciap01 <cluciap01@informatica.iesvalledeljerteplasencia.es>
2025-11-04 17:08:40 +0100

    Tema4-Ejercicio14multiplos3

--- Tema4/ejercicio14multiplos3/src/ejercicio14multiplos3/Ejercicio14multiplos3.java
+++ Tema4/ejercicio14multiplos3/src/ejercicio14multiplos3/Ejercicio14multiplos3.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejercicio14multiplos3;
+
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +12,34 @@ public class Ejercicio14multiplos3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int numero = pedirNumero();
+        mostrarMultiplosDeTres(numero);
     }
-    
+
+    // Método para pedir el número al usuario y comprobar que es mayor que 0
+    public static int pedirNumero() {
+        Scanner entrada = new Scanner(System.in);
+        int n;
+        do {
+            System.out.print("Introduce un número mayor que 0: ");
+            n = entrada.nextInt();
+            if (n <= 0) {
+                System.out.println("Error: el número debe ser mayor que 0.");
+            }
+        } while (n <= 0);
+        return n;
+    }
+
+    // 
+    public static void mostrarMultiplosDeTres(int limite) {
+        int contador = 0;
+        System.out.println("\nMúltiplos de 3 entre 1 y " + limite + ":");
+        for (int i = 1; i <= limite; i++) {
+            if (i % 3 == 0) {
+                System.out.print(i + " ");
+                contador++;
+            }
+        }
+        System.out.println("Total de múltiplos de 3: " + contador);
+    }
 }
