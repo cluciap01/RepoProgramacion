4bcb03d120580ccb1cddd1f3d802c7e8fdfca702
cluciap01 <cluciap01@informatica.iesvalledeljerteplasencia.es>
2025-11-10 18:02:02 +0100

    Tema5-Ejercicio1

---
+++ Tema5/ejercicio1/src/ejercicio1/coche.java
@@ -0,0 +1,16 @@
+
+package ejercicio1;
+
+/**
+ *
+ * @author alumno
+ */
+public class coche {
+    private String marca;
+    private String modelo;
+    private String color;
+    private int velocidad;
+    private boolean motorEncendido;
+}
+
+
---
+++ Tema5/ejercicio1/src/ejercicio1/test.java
@@ -0,0 +1,14 @@
+package ejercicio1;
+
+/**
+ *
+ * @author alumno
+ */
+public class test {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
