048515a9e53b68b5211d8136243535f3b60ece35
cluciap01 <cluciap01@informatica.iesvalledeljerteplasencia.es>
2025-11-24 17:57:44 +0100

    CorreccionEjercicio23

--- Tema5/ejercicio23Herencias/src/ejercicio23herencias/taxi.java
+++ Tema5/ejercicio23Herencias/src/ejercicio23herencias/taxi.java
@@ -9,21 +9,21 @@ public class taxi extends vehiculos{
     private int licencia;
    
     //Constructor por defecto
-public taxi(){
-    super();
-    this.licencia= 123;
-}
+    public taxi(){
+        super();
+        this.licencia= 0;
+    }
     
-public taxi(String matricula, String modelo , int potenciaCV , int licencia){
-    super(matricula, modelo , potenciaCV);
-    this.licencia=licencia;
-}
+    public taxi(String matricula, String modelo , int potenciaCV , int licencia){
+        super(matricula, modelo , potenciaCV);
+        this.licencia=licencia;
+    }
     //GET Y SET
-public int getLicencia(){
-    return this.licencia;
-}
+    public int getLicencia(){
+        return this.licencia;
+    }
 
-public void setLicencia(int licencia){
+    public void setLicencia(int licencia){
         this.licencia = licencia;
     }
     
