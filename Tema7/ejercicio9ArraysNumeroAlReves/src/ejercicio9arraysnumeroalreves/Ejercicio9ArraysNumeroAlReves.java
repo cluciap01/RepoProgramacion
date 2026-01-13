d6d4590879680bd7ebb454e8eb74b72073743873
cluciap01 <cluciap01@informatica.iesvalledeljerteplasencia.es>
2026-01-13 15:34:31 +0100

    ejercicio9Arrays-Tema7

---
+++ Tema7/ejercicio9ArraysNumeroAlReves/src/ejercicio9arraysnumeroalreves/Ejercicio9ArraysNumeroAlReves.java
@@ -0,0 +1,60 @@
+package ejercicio9arraysnumeroalreves;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio9ArraysNumeroAlReves {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero = pedirNumero();
+        int numCifras = contardorCifras(numero);
+        int[] cifras = dividirEnCifras(numero, numCifras);
+        mostrarAlReves(cifras);
+    }
+    
+    //Método para pedir un numero al usuario
+    public static int pedirNumero() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Introduzca un número entero: ");
+        return entrada.nextInt();
+    }
+    
+    //Método para contar el numero de cifras que tiene el número
+    public static int contardorCifras(int numero) {
+        int contador = 0;
+
+        do {
+            numero /= 10;
+            contador++;
+        } while (numero != 0);
+
+        return contador;
+    }
+    
+    //Método para dividir el numero en cifras
+    public static int[] dividirEnCifras(int numero, int numCifras) {
+        int[] cifras = new int[numCifras];
+
+        for (int i = numCifras - 1; i >= 0; i--) {
+            cifras[i] = numero % 10;
+            numero /= 10;
+        }
+
+        return cifras;
+    }
+    
+    // Método para mostrar el array al revés
+    public static void mostrarAlReves(int[] cifras) {
+        System.out.print("El numero introducido escrito al revés es el: ");
+
+        for (int i = cifras.length - 1; i >= 0; i--) {
+            System.out.print(cifras[i]);
+        }
+    }
+}
