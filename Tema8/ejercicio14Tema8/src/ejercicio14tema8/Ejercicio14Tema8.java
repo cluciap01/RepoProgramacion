982cee7e2ab8983b733259df2af717d98428bb3e
cluciap01 <cluciap01@informatica.iesvalledeljerteplasencia.es>
2026-02-02 18:02:35 +0100

    ejercicio14Tema8

--- Tema8/ejercicio14Tema8/src/ejercicio14tema8/Ejercicio14Tema8.java
+++ Tema8/ejercicio14Tema8/src/ejercicio14tema8/Ejercicio14Tema8.java
@@ -1,5 +1,7 @@
 package ejercicio14tema8;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alumno
@@ -10,7 +12,13 @@ public class Ejercicio14Tema8 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
         
+        Alumno[] datos= new Alumno[2];
+        
+        // Añado cada mueble a su zona de array
+        datos[0] = new Alumno("Pedro", 17, 6.4);
+        datos[1] = new Alumno();
     }
     
 }
