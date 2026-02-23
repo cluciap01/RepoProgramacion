88868258f0092912df19c6782e8400dcf52f3ad0
cluciap01 <cluciap01@informatica.iesvalledeljerteplasencia.es>
2026-02-23 17:54:50 +0100

    Ejercicios 2-3-Tema9

---
+++ Tema9/ejercicio2Tema9/src/ejercicio2tema9/Ejercicio2Tema9.java
@@ -0,0 +1,16 @@
+package ejercicio2tema9;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio2Tema9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+    
+}
---
+++ Tema9/ejercicio3Tema9/src/ejercicio3tema9/Ejercicio3Tema9.java
@@ -0,0 +1,16 @@
+package ejercicio3tema9;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio3Tema9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+    
+}
