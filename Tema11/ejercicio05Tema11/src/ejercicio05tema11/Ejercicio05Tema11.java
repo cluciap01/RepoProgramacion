f08f833f1ff9d519e5540de40cc94cfea8c4b575
cluciap01 <cluciap01@informatica.iesvalledeljerteplasencia.es>
2026-03-17 18:05:30 +0100

    ejercicio5Tema11

--- Tema11/ejercicio05Tema11/src/ejercicio05tema11/Ejercicio05Tema11.java
+++ Tema11/ejercicio05Tema11/src/ejercicio05tema11/Ejercicio05Tema11.java
@@ -20,8 +20,37 @@ public class Ejercicio05Tema11 {
         };
         
         Map<Integer, String> mapaMeses = new TreeMap<>();//Orden ascendente
+        mapaMeses.put(12, "Diciembre");
+        mapaMeses.put(11, "Noviembre");
+        mapaMeses.put(10, "Octubre");
+        mapaMeses.put(9, "Septiembre");
+        mapaMeses.put(8, "Agosto");
+        mapaMeses.put(7, "Julio");
+        mapaMeses.put(6, "Junio");
+        mapaMeses.put(5, "Mayo");
+        mapaMeses.put(4, "Abril");
+        mapaMeses.put(3, "Marzo");
+        mapaMeses.put(2, "Febrero");
+        mapaMeses.put(1, "Enero");
+        
+        System.out.println(mapaMeses);
+        
         Map<Integer, String> mapaOrdenado = new LinkedHashMap<>();//Orden de inserción 
         
+        mapaMeses.put(12, "Diciembre");
+        mapaMeses.put(11, "Noviembre");
+        mapaMeses.put(10, "Octubre");
+        mapaMeses.put(9, "Septiembre");
+        mapaMeses.put(8, "Agosto");
+        mapaMeses.put(7, "Julio");
+        mapaMeses.put(6, "Junio");
+        mapaMeses.put(5, "Mayo");
+        mapaMeses.put(4, "Abril");
+        mapaMeses.put(3, "Marzo");
+        mapaMeses.put(2, "Febrero");
+        mapaMeses.put(1, "Enero");
+        
+        System.out.println(mapaOrdenado);
     }
     
 }
