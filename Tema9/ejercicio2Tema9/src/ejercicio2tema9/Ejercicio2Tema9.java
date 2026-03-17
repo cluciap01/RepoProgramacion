607fedac57f16ededc0363afbca57f3b654c530d
cluciap01 <cluciap01@informatica.iesvalledeljerteplasencia.es>
2026-03-17 17:02:21 +0100

    ejercicio2Tema9

--- Tema9/ejercicio2Tema9/src/ejercicio2tema9/Ejercicio2Tema9.java
+++ Tema9/ejercicio2Tema9/src/ejercicio2tema9/Ejercicio2Tema9.java
@@ -1,5 +1,7 @@
 package ejercicio2tema9;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alumno
@@ -10,7 +12,17 @@ public class Ejercicio2Tema9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Por favor, introduzca una cadena de caracteres:");
+        String cadena = entrada.nextLine();
+        
+        System.out.println("Los caracteres de la cadena son:");
 
+        for (int i = 0; i < cadena.length(); i++) {
+            
+            System.out.println(cadena.charAt(i));
+        }
     }
     
 }
