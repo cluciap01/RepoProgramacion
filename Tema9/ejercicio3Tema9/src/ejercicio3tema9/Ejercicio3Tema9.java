2759c9a49ca845a3a5d10263448c82b470beea1a
cluciap01 <cluciap01@informatica.iesvalledeljerteplasencia.es>
2026-03-17 17:05:28 +0100

    ejercicio3Tema9

--- Tema9/ejercicio3Tema9/src/ejercicio3tema9/Ejercicio3Tema9.java
+++ Tema9/ejercicio3Tema9/src/ejercicio3tema9/Ejercicio3Tema9.java
@@ -1,5 +1,7 @@
 package ejercicio3tema9;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alumno
@@ -10,7 +12,18 @@ public class Ejercicio3Tema9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Introduzca una cadena de caracteres:");
+        String cadena = entrada.nextLine();// Para leer la línea completa
+        
+        char[] caracteres = cadena.toCharArray();
+        
+        System.out.println("Los caracteres de la cadena (uno por línea) son:");
+        
+        for (int i = 0; i < caracteres.length; i++) {
+            System.out.println(caracteres[i]);
+        }
     }
     
 }
