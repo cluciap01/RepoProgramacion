3367aac56f1b16d0688fd37436577cadf5a45506
cluciap01 <cluciap01@informatica.iesvalledeljerteplasencia.es>
2026-04-13 17:55:56 +0200

    Update Contacto.java

--- Tema12/ejercicio01Tema12/src/ejercicio01tema12/Contacto.java
+++ Tema12/ejercicio01Tema12/src/ejercicio01tema12/Contacto.java
@@ -8,19 +8,19 @@ public class Contacto {
     private String nombre;
     private int edad;
     private int numero;
-    
+    //Constructor por defecto
     public Contacto (){
         this.nombre = "";
         this.edad = 0;
         this.numero = 0;
     }
-    
+    //Constructor parametrizado
     public Contacto (String nombre, int edad, int numero){
         this.nombre = nombre;
         this.edad = edad;
         this.numero = numero;
     }
-
+    //Getters y Setters
     public String getNombre() {
         return nombre;
     }
