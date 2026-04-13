7eff2910d91406c87e2723ea71814f0c292b3ac3
cluciap01 <cluciap01@informatica.iesvalledeljerteplasencia.es>
2026-04-13 17:54:06 +0200

    ejercicio01Tema12

---
+++ Tema12/ejercicio01Tema12/src/ejercicio01tema12/Contacto.java
@@ -0,0 +1,47 @@
+package ejercicio01tema12;
+
+/**
+ *
+ * @author alumno
+ */
+public class Contacto {
+    private String nombre;
+    private int edad;
+    private int numero;
+    
+    public Contacto (){
+        this.nombre = "";
+        this.edad = 0;
+        this.numero = 0;
+    }
+    
+    public Contacto (String nombre, int edad, int numero){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+}
--- Tema12/ejercicio01Tema12/src/ejercicio01tema12/Ejercicio01Tema12.java
+++ Tema12/ejercicio01Tema12/src/ejercicio01tema12/Ejercicio01Tema12.java
@@ -1,5 +1,7 @@
 package ejercicio01tema12;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alumno
@@ -10,7 +12,37 @@ public class Ejercicio01Tema12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        Scanner entrada = new Scanner(System.in);
+        int opcion = 0;
+        
+        do {            
+            System.out.println("--- AGENDA DE CONTACTOS ---");
+            System.out.println("1.Añadir contactos.");
+            System.out.println("2.Visualizar lista de contactos.");
+            System.out.println("3.Eliminar contactos.");
+            System.out.println("4.Mostrar todos los contactos ordenados por edad.");
+            System.out.println("5.Salir de la agenda.");
+            
+            switch (opcion) {
+                case 1:
+                    añadirContactos;
+                    break;
+                case 2:
+                    visualizarContactos;
+                    break;
+                case 3:
+                    eliminarContactos;
+                    break;
+                case 4:
+                    mostrarContactos:
+                    break;
+                case 5:
+                    System.out.println("Saliendo de tu agenda de contactos...");
+                    break;
+                default:
+                    System.out.println("Error. Debe introducir un numero entre 1 y 5.");
+            }
+        } while (opcion != 5);
     }
     
 }
