ae4e2699da875d8db7b06f3cbe6d23694c078075
cluciap01 <cluciap01@informatica.iesvalledeljerteplasencia.es>
2026-04-13 17:18:11 +0200

    ejercicio01Tema12

---
+++ Tema12/ejercicio01Tema12/src/ejercicio01tema12/Ejercicio01Tema12.java
@@ -0,0 +1,16 @@
+package ejercicio01tema12;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio01Tema12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+    
+}
